f5c6d0173d68b5f27c98f4b0e090a33b0dc1ed77
Spider97 <antoniovns97@icloud.com>
2024-03-17 10:42:45 -0300

    Create variaveisOperadores.js

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,23 @@
+// 2 – Faça a declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável;
+//3 – Para cada resultado obtido, imprima o texto “Resultado da {operação} entre {num_A} e {num_B}”
+var num_A = 7;
+var num_B = 2;
+
+//Soma
+var resultadoSoma = num_A+num_B;
+console.log("Resultado da soma entre " +num_A+ " e " +num_B+ "em Js: "+resultadoSoma);
+//Subtracao
+var resultadoSub = num_A-num_B;
+console.log("Resultado da subtracao entre " +num_A+ " e " +num_B+ " em Js: "+ resultadoSub);
+
+//Multiplicação
+var resultadoMult = num_A*num_B;
+console.log("Resultado da multiplicacao entre " +num_A+ " e " +num_B+ " em Js: " + resultadoMult);
+
+//Divisão
+var resultadoDiv = num_A /num_B;
+console.log("Resultado da divisão entre " +num_A+ " e " +num_B+ " em Js: " + resultadoDiv);
+
+// Modulo
+var resultadoModulo = num_A%num_B;
+console.log("Resultado do modulo entre " +num_A+ " e " +num_B+ " em Js: " + resultadoModulo);
